e4186b2083b90d18d920d910037617c9e3d90cbf
Sergey Gerodes <sgerodes@gmail.com>
2023-05-27 19:39:36 +0200

    remove unnecessary

--- sorter.py
+++ sorter.py
@@ -1,6 +1,6 @@
 from collections import defaultdict
 
-data_folders = ['default', '1', '2']
+data_folders = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '10']
 
 lines = []
 for folder in data_folders:
